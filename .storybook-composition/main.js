8045fbe9eca228f32124c96cffbb05886bfe4bfb
Krzysztof Orkisz <krzysztof.orkisz@softwareone.com>
2021-09-28 13:21:45 +0200

    storybook-composition (not working)

---
+++ .storybook-composition/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  refs: {
+    'angular-checkbox-wrapper': {
+      title: 'Angular Checkbox Wrapper',
+      url: 'http://localhost:4401'
+    },
+    'angular-progress-bar': {
+      title: 'Angular Progress Bar',
+      url: 'http://localhost:4402'
+    }
+  }
+};
